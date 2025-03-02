26168e65c59ad2fc96c3eabe5799650a0fa59913
Hilal-11 <hilalahmadcodedev123@gmail.com>
2025-03-03 02:20:13 +0530

    boostrrap ui

--- bootstrap-react/src/App.jsx
+++ bootstrap-react/src/App.jsx
@@ -1,15 +1,35 @@
 import React from 'react';
 import Navigation from './Components/Navigation';
 import Hero from './Components/Hero';
+import Cards from './Components/Cards';
 function App() {
   return (
-    <div className="container">
+    <div>
+      {/* Header secession */}
+      <div className="container">
       <div>
         <Navigation/>
       </div>
+      {/* Hero secession */}
       <div className='container'>
         <Hero/>
       </div>
+    </div>
+    <br /> <br />
+    <div className='mt-4'
+    style={{
+      backgroundColor: '#93c5fd'
+    }}>
+
+      {/* Cards secession */}
+    <br /> <br />
+      <div className='container' 
+      style={{
+        paddingTop: '30px'
+      }}>
+        <Cards/>
+      </div>
+    </div>
     </div>
   );
 }
--- bootstrap-react/src/Components/Hero.jsx
+++ bootstrap-react/src/Components/Hero.jsx
@@ -2,15 +2,25 @@ import React from 'react'
 
 function Hero() {
   return (
-    <div className='container text-center p-4'> 
-        <div className='row'>
-            <div className='col pt-4'>
-                <h1>
+    <div className='container  p-4'> 
+        <div className='row'
+            style={{
+                paddingTop: '30px'
+            }}
+        >
+            <div className='col pt-4 text-center'>
+                <h1 className=''
+                    style={{
+                        fontSize: '4rem',
+                        fontWeight: 'bold' 
+                        
+                    }}
+                >
                     Build your next project faster with SB UI Kit Pro
                 </h1>
-                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Optio aliquam autem eaque recusandae facilis accusantium omnis, quidem molestiae eius itaque consequuntur! Provident laudantium sequi sit neque vitae ullam sed optio perspiciatis ipsam facilis porro natus alias nesciunt aliquid, recusandae est nihil voluptates saepe suscipit at debitis enim? Cumque, quae iure? Quasi sequi laboriosam vero quisquam nemo quibusdam totam quo suscipit?</p>
-                <div className='d-flex justify-content-evenly'>
-                    <button type="button" class="p-2 g-col-6 px-4 btn btn-primary btn-lg font-weight-bold">Explore Pages</button>
+                <p className=' pt-4'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Optio aliquam autem eaque recusandae facilis accusantium omnis, quidem molestiae eius itaque consequuntur! Provident laudantium sequi sit neque vitae ullam sed optio perspiciatis ipsam facilis porro natus alias nesciunt aliquid, recusandae est nihil voluptates saepe suscipit at debitis enim? Cumque, quae iure? Quasi sequi laboriosam vero quisquam nemo quibusdam totam quo suscipit?</p>
+                <div className='d-flex justify-content-evenly pt-4'>
+                    <button type="button" class="p-2 g-col-6 px-4 btn btn-primary btn-lg fw-bold">Explore Pages</button>
                     <button type="button" class=" px-4 btn-secondary btn btn-lg font-weight-bold">Documentation</button>
                 </div>
             </div>
